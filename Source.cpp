2ed2c509bd2aa2b3d78cdfb49412a4413e1b3f14
Robert Schumacher <ras0219@outlook.com>
2016-08-29 02:24:09 -0700

    rewrite with pipelining

--- Source.cpp
+++ Source.cpp
@@ -1,157 +1,238 @@
 #include <iostream>
 #include <iomanip>
+#include <array>
+#include <cassert>
 
 using namespace std;
 
-enum reg_idx : uint32_t
+enum instruction_t : uint16_t
 {
-    REG_1 = 0,
-    REG_2 = 1,
-    REG_3 = 2,
-    REG_4 = 3,
-};
-enum op_idx : uint32_t
-{
-    OP_AND,
-    OP_OR,
-    OP_XOR,
-    OP_LSHIFTIMMONE,
-    OP_LSHIFTIMMZERO,
+    OP_AND = 0x1000,
+    OP_OR = 0x2000,
+    OP_XOR = 0x3000,
+    OP_LS1 = 0x4000,
+    OP_LS0 = 0x5000,
+
+    OP_MASK = 0xF000,
+
+    RA_IMM = 0x0010,
+    RA_CONSUME = 0x0008,
+    RA_MASK = 0x001F,
+#define RA_IMMVAL(X) (RA_IMM | (X))
+#define RA_IDX(X) (X)
+
+    RB_IMM = 0x0010 << 5,
+    RB_CONSUME = 0x0008 << 5,
+    RB_MASK = 0x001F << 5,
+#define RB_IMMVAL(X) (RB_IMM | ((X) << 5))
+#define RB_IDX(X) ((X) << 5)
+
+    RO_DROP = 0x0000,
+    RO_PC = 0x0800,
+    RO_STK = 0x0400,
+    RO_MASK = 0x0C00,
 };
 
-struct InputLines
+struct Instruction
 {
-    constexpr InputLines() : InputLines(OP_AND, REG_1, REG_1, REG_1, 0) {}
-    constexpr InputLines(op_idx o, reg_idx r1, reg_idx r2, reg_idx ro, uint32_t i = 0)
-        : op(o)
-        , reg1(r1)
-        , reg2(r2)
-        , regOut(ro)
-        , imm(i)
-    {}
+    uint16_t instr;
 
-    op_idx op : 3;
-    reg_idx reg1 : 2;
-    reg_idx reg2 : 2;
-    reg_idx regOut : 2;
-    uint32_t imm : 11;
+    constexpr uint16_t regA_consume() const { return instr & RA_CONSUME; }
+    constexpr uint16_t regA_imm() const { return instr & RA_IMM; }
+    constexpr uint16_t regB_consume() const { return instr & RB_CONSUME; }
+    constexpr uint16_t regB_imm() const { return instr & RB_IMM; }
+    constexpr uint16_t regA() const { return instr & RA_MASK; }
+    constexpr uint16_t regB() const { return (instr & RB_MASK) >> 5; }
+    constexpr uint16_t op() const { return instr & OP_MASK; }
+    constexpr uint16_t out() const { return instr & RO_MASK; }
 };
 
 struct ProcState
 {
-    uint32_t reg1 = 0;
-    uint32_t reg2 = 0;
-    uint32_t reg3 = 0;
-    uint32_t reg4 = 0;
+    uint16_t ip = 0;
+    std::array<uint16_t, 16> stack = { 0 };
 
-    uint32_t decode_reg(reg_idx x) const
+    uint16_t opval1 = 0;
+    uint16_t opval2 = 0;
+
+    uint16_t opresult = 0;
+
+    uint16_t regval(uint16_t r) const
     {
-        switch (x)
-        {
-        case REG_1: return reg1;
-        case REG_2: return reg2;
-        case REG_3: return reg3;
-        case REG_4: return reg4;
-        default: abort();
-        }
+        if (r & 0x10)
+            return r & 0xF;
+        else
+            return stack[r & 0x7];
     }
 
-    uint32_t& decode_reg(reg_idx x)
+    void simulateStep(const Instruction& in, ProcState& out) const
     {
-        switch (x)
-        {
-        case REG_1: return reg1;
-        case REG_2: return reg2;
-        case REG_3: return reg3;
-        case REG_4: return reg4;
-        default: abort();
-        }
-    }
 
-    void simulateStep(const InputLines& in, ProcState& out) const
-    {
-        auto v1 = decode_reg(in.reg1);
-        auto v2 = decode_reg(in.reg2);
-        uint32_t result;
-        switch (in.op)
+        out.opval1 = regval(in.regA());
+        out.opval2 = regval(in.regB());
+
+        switch (in.op())
         {
-        case OP_AND: result = v1 & v2; break;
-        case OP_OR: result = v1 | v2; break;
-        case OP_XOR: result = v1 ^ v2; break;
-        case OP_LSHIFTIMMONE: result = ((v1+1) << in.imm) - 1; break; // fill with 1s
-        case OP_LSHIFTIMMZERO: result = v1 << in.imm; break; // fill with 0s
+        case OP_AND: out.opresult = opval1 & opval2; break;
+        case OP_OR: out.opresult = opval1 | opval2; break;
+        case OP_XOR: out.opresult = opval1 ^ opval2; break;
+        case OP_LS1: out.opresult = ((opval1 + 1) << opval2) - 1; break; // fill with 1s
+        case OP_LS0: out.opresult = opval1 << opval2; break; // fill with 0s
         default:
             abort();
         }
 
-        out = *this;
-        out.decode_reg(in.regOut) = result;
+        if (in.out() == RO_PC)
+            out.ip = opresult;
+        else
+            out.ip = ip + 1;
+
+        // Overall, this should copy the stack over while removing all "consumed" registers and inserting if output to stack
+        auto out_b = out.stack.begin();
+        auto in_b = stack.begin();
+        if (in.out() == RO_STK)
+        {
+            *out_b++ = opresult;
+        }
+        for (int x = 0;x < 7;++x)
+        {
+            if (!in.regA_imm() && in.regA_consume() && in.regA() == x)
+            {
+                in_b++;
+                continue;
+            }
+            if (!in.regB_imm() && in.regB_consume() && in.regB() == x)
+            {
+                in_b++;
+                continue;
+            }
+            *out_b++ = *in_b++;
+        }
+        out_b = std::copy(in_b, in_b + min(stack.end() - in_b, out.stack.end() - out_b), out_b);
+        std::fill(out_b, out.stack.end(), 0);
     }
 
     void print() const
     {
-        cout << hex << "regs: {" << reg1 << ", " << reg2 << ", " << reg3 << ", " << reg4 << "}\n";
+        cout << hex;
+        cout << '[' << setw(4) << opval1 << ' ' << setw(4) << opval2 << ' ' << setw(4) << opresult << "] ";
+        for (auto&& s : stack)
+        {
+            cout << setw(4) << s << ' ';
+        }
+        cout << "\n";
     }
 };
 
 
 int main()
 {
-    constexpr size_t STAGES = 25;
-    ProcState proc[STAGES + 1];
-    proc[0].reg1 = 0xF0F1;
-    proc[0].reg2 = 0x0F0F;
-
-    constexpr auto x = 0xF0F1 + 0x0F0F - 0x10000;
-
     // Add reg1 to reg2
     // reg3 is P, reg4 is G
-    constexpr InputLines code[STAGES] = {
-        InputLines(OP_XOR, REG_1, REG_2, REG_3),
-        InputLines(OP_AND, REG_1, REG_2, REG_4),
-        InputLines(OP_AND, REG_3, REG_3, REG_2),
-        // G = ...
-        InputLines(OP_LSHIFTIMMZERO, REG_4, REG_4, REG_1, 1),
+    //constexpr Instruction code[] = {
+    //    InputLines(OP_XOR, REG_1, REG_2, REG_3),
+    //    InputLines(OP_AND, REG_1, REG_2, REG_4),
+    //    InputLines(OP_AND, REG_3, REG_3, REG_2),
+    //    // G = ...
+    //    InputLines(OP_LS0, REG_4, REG_4, REG_1, 1),
 
-        InputLines(OP_AND, REG_1, REG_3, REG_1),
-        InputLines(OP_OR, REG_1, REG_4, REG_4),
-        // P = ...
-        InputLines(OP_LSHIFTIMMONE, REG_3, REG_3, REG_1, 1),
-        InputLines(OP_AND, REG_3, REG_1, REG_3),
-        // G = ...
+    //    InputLines(OP_AND, REG_1, REG_3, REG_1),
+    //    InputLines(OP_OR, REG_1, REG_4, REG_4),
+    //    // P = ...
+    //    InputLines(OP_LS1, REG_3, REG_3, REG_1, 1),
+    //    InputLines(OP_AND, REG_3, REG_1, REG_3),
+    //    // G = ...
 
-        InputLines(OP_LSHIFTIMMZERO, REG_4, REG_4, REG_1, 2),
-        InputLines(OP_AND, REG_1, REG_3, REG_1),
-        InputLines(OP_OR, REG_1, REG_4, REG_4),
-        // P = ...
-        InputLines(OP_LSHIFTIMMONE, REG_3, REG_3, REG_1, 2),
+    //    InputLines(OP_LS0, REG_4, REG_4, REG_1, 2),
+    //    InputLines(OP_AND, REG_1, REG_3, REG_1),
+    //    InputLines(OP_OR, REG_1, REG_4, REG_4),
+    //    // P = ...
+    //    InputLines(OP_LS1, REG_3, REG_3, REG_1, 2),
 
-        InputLines(OP_AND, REG_3, REG_1, REG_3),
-        // G = ...
-        InputLines(OP_LSHIFTIMMZERO, REG_4, REG_4, REG_1, 4),
-        InputLines(OP_AND, REG_1, REG_3, REG_1),
-        InputLines(OP_OR, REG_1, REG_4, REG_4),
+    //    InputLines(OP_AND, REG_3, REG_1, REG_3),
+    //    // G = ...
+    //    InputLines(OP_LS0, REG_4, REG_4, REG_1, 4),
+    //    InputLines(OP_AND, REG_1, REG_3, REG_1),
+    //    InputLines(OP_OR, REG_1, REG_4, REG_4),
 
-        // P = ...
-        InputLines(OP_LSHIFTIMMONE, REG_3, REG_3, REG_1, 4),
-        InputLines(OP_AND, REG_3, REG_1, REG_3),
-        // G = ...
-        InputLines(OP_LSHIFTIMMZERO, REG_4, REG_4, REG_1, 8),
-        InputLines(OP_AND, REG_1, REG_3, REG_1),
-        InputLines(OP_OR, REG_1, REG_4, REG_4),
+    //    // P = ...
+    //    InputLines(OP_LS1, REG_3, REG_3, REG_1, 4),
+    //    InputLines(OP_AND, REG_3, REG_1, REG_3),
+    //    // G = ...
+    //    InputLines(OP_LS0, REG_4, REG_4, REG_1, 8),
+    //    InputLines(OP_AND, REG_1, REG_3, REG_1),
+    //    InputLines(OP_OR, REG_1, REG_4, REG_4),
 
-        // P = ...
-        InputLines(OP_LSHIFTIMMONE, REG_3, REG_3, REG_1, 8),
-        InputLines(OP_AND, REG_3, REG_1, REG_3),
-        // C = G (nop)
-        // S = P ^ Cprev
-        InputLines(OP_LSHIFTIMMZERO, REG_4, REG_4, REG_1, 1),
-        InputLines(OP_XOR, REG_2, REG_1, REG_1),
+    //    // P = ...
+    //    InputLines(OP_LS1, REG_3, REG_3, REG_1, 8),
+    //    InputLines(OP_AND, REG_3, REG_1, REG_3),
+    //    // C = G (nop)
+    //    // S = P ^ Cprev
+    //    InputLines(OP_LS0, REG_4, REG_4, REG_1, 1),
+    //    InputLines(OP_XOR, REG_2, REG_1, REG_1),
+    //};
+
+#define RA_POP(X) (RA_IDX(X) | RA_CONSUME)
+    Instruction program[] = {
+        // 5 => push $A
+        RA_IMMVAL(5)  | RB_IMMVAL(5)  | OP_OR  | RO_DROP,
+        // 3 => push $B
+        RA_IMMVAL(3)  | RB_IMMVAL(3)  | OP_OR  | RO_DROP,
+        // bubble
+        RA_IMMVAL(0)  | RB_IMMVAL(0)  | OP_OR  | RO_STK,
+        // bubble
+        RA_IMMVAL(0)  | RB_IMMVAL(0)  | OP_OR  | RO_STK,
+
+        // $B xor $A => push $Pi
+        RA_IDX(0)     | RB_IDX(1)     | OP_OR  | RO_DROP,
+        // $B and $A => push $Gi
+        RA_IDX(0)     | RB_IDX(1)     | OP_XOR | RO_DROP,
+        // bubble
+        RA_IDX(0)     | RB_IDX(1)     | OP_AND | RO_STK,
+
+        // $Pi <<o 1 => push $Pp
+        RA_IDX(0)     | RB_IMMVAL(1)  | OP_OR  | RO_STK,
+        // $Gi <<z 1 => push $Gp
+        RA_IDX(0)     | RB_IMMVAL(1)  | OP_LS1 | RO_DROP,
+        // bubble
+        RA_IDX(0)     | RB_IDX(1)     | OP_LS0 | RO_STK,
+        // pop $Pp and $Pi => push $P
+        RA_POP(0)     | RB_IDX(2)     | OP_OR  | RO_STK,
+        // pop $Gp and $Pi => push $Gtmp
+        // bubble
+        // bubble
+        // pop $Gtmp or pop $Gi => push $G
+        // bubble
+
+        // $P <<o 2 => push $Pp
+        // $G <<z 2 => push $Gp
+        // bubble
+        // pop $Pp and $Pi => push $P
+        RA_POP(0) | RB_IDX(2) | OP_OR | RO_STK,
+        // pop $Gp and $Pi => push $Gtmp
+        // bubble
+        // bubble
+        // pop $Gtmp or pop $Gi => push $G
+        // bubble
+        // bubble
+
+        RA_IMMVAL(3)  | RB_IMMVAL(3)  | OP_AND | RO_DROP,
+        RA_IMMVAL(3)  | RB_IMMVAL(3)  | OP_OR  | RO_STK,
+        RA_IMMVAL(3)  | RB_IMMVAL(3)  | OP_OR  | RO_DROP,
+        RA_IMMVAL(3)  | RB_IMMVAL(3)  | OP_OR  | RO_DROP,
+        RA_IMMVAL(3)  | RB_IMMVAL(3)  | OP_OR  | RO_DROP,
+        RA_IMMVAL(3)  | RB_IMMVAL(3)  | OP_OR  | RO_DROP,
     };
+    constexpr size_t STAGES = sizeof(program) / sizeof(program[0]);
+    ProcState proc[STAGES + 1];
+
+    cout << setw(4) << 0 << ") ";
+    proc[0].print();
+
     for (int x = 0;x < STAGES;++x)
     {
-        proc[x].simulateStep(code[x], proc[x + 1]);
-        cout << x << ") ";
+        proc[x].simulateStep(program[x], proc[x + 1]);
+        cout << setw(4) << x+1 << ") ";
         proc[x + 1].print();
     }
 
